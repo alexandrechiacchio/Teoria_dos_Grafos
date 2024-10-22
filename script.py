8cb4e43154cb86830e203fe8addfdc52f2f0f889
Alexandre Chiacchio <alexandrechiacchio30@gmail.com>
2024-10-22 10:33:55 -0300

    added script to re run everything easily

--- script.py
+++ script.py
@@ -1,9 +1,18 @@
 from subprocess import run
-
+from multiprocessing import Process
+from threading import Thread
 
 # Compile the C++ program
 run(["g++", "main.cpp", "-O3", "-o", "a"], check=True)
 
 # Run the compiled program with different arguments
+processes = []
 for i in range(1, 6):
-    run([f"./a", "grafo_W_" + str(i) + ".txt"], check=True)
+    processes.append(Process(target=run, args=(["./a", 'grafo_W_' + str(i) + '.txt'],), kwargs={"check": True}))
+    processes[-1].deamon = True
+
+for i in processes:
+    i.start()
+
+for i in processes:
+    i.join()
